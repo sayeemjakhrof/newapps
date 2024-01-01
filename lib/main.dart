6be4a4da389f9b8671962c8350683e6659084a68
sayeemjakhrof <47790968+sayeemjakhrof@users.noreply.github.com>
2024-01-01 16:00:26 +0600

    Login page create
    Signed-off-by: sayeemjakhrof <47790968+sayeemjakhrof@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,108 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(const Myapp());
-
+  runApp( Myapps());
 }
-
-class Myapp extends StatelessWidget{
-  const Myapp({super.key});
-
-
-  @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      title: 'NewApp',
-      debugShowCheckedModeBanner: false,
-      home: Button(),
-    );
-  }
-}
-class Button extends StatelessWidget {
-  const Button({super.key});
+class Myapps extends StatelessWidget {
+  Myapps({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.large(onPressed: (){},),
-      appBar: AppBar(
-        
-        //leading: const Icon(Icons.menu),
-        toolbarHeight: 70,
-        backgroundColor: Colors.amber.shade600,
-        title: Center(
-            child: Text('Button',
-              style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-              ),
-            ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-         Center(
-             child: ElevatedButton(
-               autofocus: true,
-                 onPressed: (){
-             print("on press");
-         },
-         onLongPress: (){
-           print("Deleting button");
-         },
-             child: const Text('ElevatedButton', style: TextStyle(fontWeight: FontWeight.bold,
-             fontSize: 30,
-             color: Colors.brown
-         ),
-         ),
-         ),
-         ),
-          const SizedBox(height: 10,),
-          Center(
-              child: TextButton(
-                onPressed: (){},
-                child: const Text('This is Text Button',
-                  style: TextStyle(
-                      color: Colors.amber, fontSize: 30, 
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline
-                  ),
-                ),
-              ),
-          ),
-          const SizedBox(height: 10,),
-          Center(
-            child: IconButton(
-              icon: Icon(Icons.add, size: 100, color: Colors.amberAccent,),
-              onPressed: (){
-                print('Timer checker');
-              },
-            ),
-          ),
-          const SizedBox(height: 10,),
-
-        ],
-      ),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+            color: Colors.amberAccent.shade700),
+            primarySwatch: Colors.orange,
+            canvasColor: Colors.brown),
+      title: 'Login Page',
+      home: Login_page(),
     );
   }
 }
+class Login_page extends StatelessWidget{
+  var email = TextEditingController();
+  var pass = TextEditingController();
+    Login_page({super.key});
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+                'Login Page',
+                style: TextStyle(
+                    color: Colors.black,
+                fontSize: 24,fontWeight: FontWeight.bold
+                ),),
+        ),
+      ),
+        body: Center(
+          child: Container(width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                //height: ,
+                child: Text('Login', style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold, fontSize: 40),),
+              ),
+              TextField(
+                c
+                controller: email,
+                decoration: InputDecoration(
+                  hintText: 'Enter your Email',
+                  prefixIcon: Icon(Icons.mail, color: Colors.orangeAccent, size: 20,),
+                  //prefixText:'Please enter our Email',
+                    helperText: 'Please Enter our Email',
+                  helperStyle: TextStyle(color: Colors.orange.shade400),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(11),
+                    borderSide: BorderSide(color: Colors.orangeAccent, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(11),
+                    borderSide: BorderSide(color: Colors.brown, width: 2),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(11),
+                    borderSide: BorderSide(color: Colors.amberAccent, width: 2)
+                  )
+                ),
 
-
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                obscureText: true,
+                obscuringCharacter: "*",
+                controller: pass,
+                readOnly: false,
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                    prefixIcon: Icon(Icons.password, color: Colors.orangeAccent, size: 20,),
+                    helperText: 'Please Enter our password',
+                    helperStyle: TextStyle(color: Colors.orange.shade400),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(color: Colors.orangeAccent, width: 2),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(color: Colors.brown, width: 2),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(11),
+                        borderSide: BorderSide(color: Colors.deepPurple, width: 2)
+                    )
+                ),
+              ),
+              IconButton(onPressed: (){
+               String _Email = email.text;
+               String _pass = pass.text;
+               print('Email Address $_Email & password is $_pass');
+              }, icon: Text('login'), color: Colors.orange,focusColor: Colors.orange,)
+            ],
+          ),),
+        ),
+      );
+  }
+  }

cbff492fb5ee251115ed97ddd902840e394fbf2e
sayeemjakhrof <47790968+sayeemjakhrof@users.noreply.github.com>
2024-01-16 18:50:45 +0600

    ListMapAdvence
    Signed-off-by: sayeemjakhrof <47790968+sayeemjakhrof@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,188 +6,66 @@ void main() {
 
 class MyApps extends StatelessWidget {
   MyApps({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Navigator ',
-      theme: ThemeData(
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.blue))),
-          primaryColor: Colors.cyanAccent,
-          appBarTheme: AppBarTheme(color: Colors.blue)),
-      home: NavigatorScreen(),
-    );
-  }
-}
-
-class NavigatorScreen extends StatelessWidget {
-  NavigatorScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        //leading: Drawer(child: IconButton(onPressed: (){}, icon: Icon(Icons.android_outlined),),),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.add,
-                color: Colors.white,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.account_box,
-                color: Colors.white,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.add_alarm,
-                color: Colors.white,
-              )),
-        ],
-        title: Center(
-          child: Text(
-            'HomeScreen',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+          colorScheme: ColorScheme.dark(
+            background: Colors.black12,
           ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return SettingScreen();
-                  }));
-                },
-                child: Text(
-                  'Open Setting Button',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                )),
-            Divider(
-              color: Colors.grey.shade200,
-              height: 20,
-              indent: 120,
-              endIndent: 120,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>OrderScreen()));
-                },
-                child: Text(
-                  'Open Order Button',
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                )),
-          ],
-        ),
-      ),
+          appBarTheme: AppBarTheme(
+              titleTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+              centerTitle: true,
+              color: Colors.lime[800],
+              shadowColor: Colors.black12)),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+        titleTextStyle: TextStyle(
+            color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+        color: Colors.orange,
+        centerTitle: true,
+        shadowColor: Colors.black,
+      )),
+      home: HomeScreen(),
     );
   }
 }
 
-class SettingScreen extends StatelessWidget {
-  SettingScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Setting Screen',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> OrderScreen()));
-              },
-              child: Text(
-                'Open Order Button',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-            Divider(
-              color: Colors.grey.shade200,
-              height: 20,
-              indent: 120,
-              endIndent: 120,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                'Back to Home',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+  List ArrayList = [
+    {'name': 'Dominic', 'age': '18', 'Nationality': 'Austelia'},
+    {'name': 'jao', 'age': '20', 'Nationality': 'newziland'},
+    {'name': 'carry', 'age': '23', 'Nationality': 'Italy'},
+    {'name': 'john', 'age': '38', 'Nationality': 'America'},
+    {'name': 'John show', 'age': '34', 'Nationality': 'America'},
+    {'name': 'Sayeem Jakhrof', 'age': '35', 'Nationality': 'Bangladesh'}
+  ];
 
-class OrderScreen extends StatelessWidget {
-  OrderScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Order Screen',
-          style: TextStyle(color: Colors.white, fontSize: 24),
+        appBar: AppBar(
+          title: Text('Home'),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>NavigatorScreen()),
-                        (route) => false);
-              },
-              child: Text(
-                'Open Setting Button',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-            Divider(
-              color: Colors.grey.shade200,
-              height: 20,
-              indent: 120,
-              endIndent: 120,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                'Back to Home',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        body: Container(
+          child: ListView(
+              children: ArrayList.map((value) => ListTile(
+                    titleTextStyle: TextStyle(fontSize: 24),
+                    iconColor: Colors.grey,
+                    leading: Icon(Icons.circle),
+                    title: Text(value['name'].toString()),
+                    subtitle: Text(value['Nationality'].toString()),
+                    trailing:
+                        CircleAvatar(
+                            child: Text(value['age'].toString())),
+                  )).toList()),
+        ));
   }
 }
